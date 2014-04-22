b576b4e1c166241c4c343c40384b915067762bd3
Peter Williams <peter@newton.cx>
2014-04-21 23:11:34 -0400

    Node-based HTTP server testbed as promised.

---
+++ test-server.js
@@ -0,0 +1,9 @@
+var http = require ('http');
+var connect = require ('connect');
+
+var port = 17394;
+var maxage = 8640000000; // 100 d in ms
+var app = connect ().use (connect.static (__dirname, {maxAge: maxage}));
+
+http.createServer (app).listen (port);
+console.log ('listening on port ' + port);
--- testbed.js
+++ testbed.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var url = 'http://newton.cx/~peter/files/tb-default.zip';
+var url = 'texbundles/default.zip';
 WEBTEX.Web.make_random_access_url (url).then (function (rau) {
     console.log ('whoa ' + rau._size);
 });
