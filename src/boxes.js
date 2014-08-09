0a4df341535dccc659c0eaf6fb9b39a1415e719f
Peter Williams <peter@newton.cx>
2014-08-09 18:15:25 -0400

    src/boxes.js: kill commented-out code that won't be coming back

--- src/boxes.js
+++ src/boxes.js
@@ -71,33 +71,6 @@ var Box = (function Box_closure () {
 	return b;
     };
 
-    // Some compatible API with Value for convenience.
-
-    // proto.is_nonzero = function Box_is_nonzero () {
-    // 	// The way TeX handles things, an all-zero non-void box is best
-    // 	// considered nonzero.
-    // 	return this.type != BT_VOID;
-    // };
-
-    // proto.as_serializable = function Box_as_serializable () {
-    // 	if (this.list.length)
-    // 	    throw new TexInternalException ('can\'t serialize box lists yet');
-    // 	return [this.btype,
-    // 		this.width.as_serializable (),
-    // 		this.height.as_serializable (),
-    // 		this.depth.as_serializable ()];
-    // };
-
-    // Box.deserialize = function Box_deserialize (data) {
-    // 	var b = new Box (BT_VOID);
-    // 	b.btype = parseInt (data[0], 10);
-    // 	b.width = Dimen.deserialize (data[1]);
-    // 	b.height = Dimen.deserialize (data[2]);
-    // 	b.depth = Dimen.deserialize (data[3]);
-    // 	b.list = []; // XXX temp?
-    // 	return b;
-    // };
-
     return Box;
 }) ();
 
