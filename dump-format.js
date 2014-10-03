15e3000a572dade8cdd3d182ae3244e05cb7520a
Peter Williams <peter@newton.cx>
2014-10-03 19:40:47 -0400

    dump-format.js: tidy up a bit

--- dump-format.js
+++ dump-format.js
@@ -7,19 +7,18 @@ if (process.argv.length < 5) {
 }
 
 var webtex = require (process.argv[2]);
-
 var patchdir = process.argv[3];
-var lb = webtex.LineBuffer.new_static (['\\input ' + process.argv[4]]);
-var bundlepath = 'misc/minimal-bundle-tl2013.zip';
-var debug_trace = false;
-var debug_input_lines = false;
+var fmtname = process.argv[4];
 
-var eng = WEBTEX.setup_process_format ({
-    jobname: process.argv[4],
-    initial_linebuf: lb,
-    bundlepath: bundlepath,
-    debug_trace: debug_trace,
-    debug_input_lines: debug_input_lines,
+// The way we set up initial_linebuf allows us to trigger patching of the
+// format file.
+
+var eng = webtex.setup_process_format ({
+    jobname: fmtname,
+    initial_linebuf: webtex.LineBuffer.new_static (['\\input ' + fmtname]),
+    bundlepath: 'misc/minimal-bundle-tl2013.zip',
+    debug_trace: false,
+    debug_input_lines: false,
 });
 
 eng.iostack.push (new webtex.Node.FSIOLayer ('__wtpatches__/', patchdir));
