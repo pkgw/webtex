fd3df6033b7aeac61a50db96404f16c4dff3442e
Peter Williams <peter@newton.cx>
2014-10-04 00:28:48 -0400

    testbed.js: tidy up

--- testbed.js
+++ testbed.js
@@ -1,20 +1,15 @@
 'use strict';
 
 var targdiv = document.getElementById ('webtex');
-
-var bundleurl = 'latest.zip'; // note: relative to *worker's* URL
-var inputurl = '../test/tex/latex-minimal.tex'; // ditto
-var dump_bpath = 'latex.dump.json';
 var worker_url = 'build/browser-worker-webtex.js';
-var debug_trace = false;
-var debug_input_lines = false;
+var inputurl = '../test/tex/latex-minimal.tex'; // note: relative to *worker's* URL
 
 var dr = new Webtex.DOMRenderer (worker_url, targdiv);
 dr.launch_parse ({
     jobname: 'plain',
     inputurl: inputurl,
-    bundleurl: bundleurl,
-    dump_bpath: dump_bpath,
-    debug_trace: debug_trace,
-    debug_input_lines: debug_input_lines,
+    bundleurl: 'latest.zip', // note: relative to *worker's* URL
+    dump_bpath: 'latex.dump.json',
+    debug_trace: false,
+    debug_input_lines: false,
 });
