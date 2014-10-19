03da6cf18b5d2037c02f395f17c689dd2dbfd1b8
Peter Williams <peter@newton.cx>
2014-10-19 17:42:08 -0400

    Remove the Scaled type.

--- src/values.js
+++ src/values.js
@@ -66,215 +66,6 @@ var TexInt = (function TexInt_closure () {
 }) ();
 
 
-var Scaled = (function Scaled_closure () {
-    var SC_HALF  = 0x8000,     // 2**15 = 32768      = '100000
-        SC_UNITY = 0x10000,    // 2**16 = 65536      = '200000
-        SC_TWO   = 0x20000,    // 2**17 = 131072     = '400000
-        SC_MAX   = 0x40000000, // 2**30 = 1073741824 = '10000000000
-        UNSCALE  = Math.pow (2, -16);
-
-    // These objects are immutable.
-    function Scaled (value) {
-	if (value instanceof Scaled)
-	    this.value_S = value.value_S;
-	else
-	    this.value_S = nlib.from_raw__I_S (value);
-    }
-
-    inherit (Scaled, Value);
-    var proto = Scaled.prototype;
-
-    // Math library.
-
-    function div (a, b) {
-	return a / b >> 0;
-    }
-
-    function mult_and_add (n, x, y, maxanswer) {
-	// n: tex-int
-	// x, y, retval: Scaled
-	// maxanswer: js int
-
-	if (n < 0) {
-	    var xv = -x.value_S;
-	    n = -n;
-	} else {
-	    var xv = x.value_S;
-	}
-
-	if (n == 0)
-	    return y;
-
-	var yv = y.value_S;
-
-	if (xv <= div (maxanswer - yv, n) && -xv <= div (maxanswer + yv, n))
-	    return new Scaled (n * xv + yv);
-	throw new TexRuntimeError ('over/underflow in mult+add');
-    }
-
-    Scaled.new_from_parts__II_S = function Scaled_new_from_parts__II_S (nonfrac_I, frac_I) {
-	return new Scaled (nlib.from_parts__II_S (nonfrac_I, frac_I));
-    };
-
-    Scaled.new_parts_product__IIII_S =
-	function Scaled_new_parts_product__IIII_S (num, denom, nonfrac, frac) {
-	    // equivalent to `new_from_parts (nonfrac, frac) * (num/denom)` with
-	    // better precision than you'd get naively.
-	    var s = new Scaled (nonfrac);
-	    var t = s.times_n_over_d (num, denom); // -> [result, remainder]
-	    frac = div ((num * frac + SC_UNITY * t[1]), denom);
-	    nonfrac = t[0].value_S + div (frac, SC_UNITY);
-	    frac = frac % SC_UNITY;
-	    return Scaled.new_from_parts__II_S (nonfrac, frac);
-	};
-
-    Scaled.new_from_decimals =
-	function Scaled_new_from_decimals (digarray) {
-	    var a = 0;
-	    while (digarray.length)
-		a = div (a + digarray.pop () * SC_TWO, 10);
-	    return div (a + 1, 2);
-	};
-
-    proto.times_n_plus_y = function Scaled_times_n_plus_y (n, y) {
-	// OO interpretation of nx_plus_y.
-	// n: tex-int
-	// y: Scaled
-	// returns: Scaled(n*this+y)
-
-	n = nlib.maybe_unbox__O_I (n);
-	if (!(y instanceof Scaled))
-	    throw new TexInternalError ('nx+y called with non-Scaled y: %o', y);
-	return mult_and_add (n, this, y, SC_MAX - 1);
-    };
-
-    proto.times_n_over_d = function Scaled_times_n_over_d (n, d) {
-	// OO interpretation of xn_over_d.
-	// n: tex-int
-	// d: tex-int
-	// returns: [Scaled(result), Scaled(remainder)]
-	//   where the remainder is relevant if the low-significance digits
-	//   of (this*n/d) must be rounded off.
-
-	n = nlib.maybe_unbox__O_I (n);
-	d = nlib.maybe_unbox__O_I (d);
-
-	var positive = (this.value_S >= 0);
-	if (positive)
-	    var xv = this.value_S;
-	else
-	    var xv = -this.value_S;
-
-	var t = (xv % SC_HALF) * n;
-	var u = div (xv, SC_HALF) * n + div (t, SC_HALF);
-	var v = (u % d) * SC_HALF + (t % SC_HALF);
-
-	if (div (u, d) > SC_HALF)
-	    throw new TexRuntimeError ('over/underflow in xn_over_d');
-
-	var w = SC_HALF * div (u, d) + div (v, d);
-
-	if (positive)
-	    return [new Scaled (w), new Scaled (v % d)];
-	return [new Scaled (-w), new Scaled (-(v % d))];
-    };
-
-    proto.over_n = function Scaled_over_n (n) {
-	// OO version of x_over_n.
-	// n: tex-int
-	// returns: [Scaled(x/n), Scaled(remainder)]
-	//   where the remainder is relevant if the low-significance digits
-	//   of (this/n) must be rounded off.
-
-	n = nlib.maybe_unbox__O_I (n);
-	if (n.value_I == 0)
-	    throw new TexRuntimeError ('really, dividing by 0?');
-
-	var negative = false;
-
-	if (n < 0) {
-	    var xv = -this.value_S;
-	    n = -n;
-	    negative = true;
-	} else {
-	    var xv = this.value_S;
-	}
-
-	if (xv >= 0) {
-	    var rv = div (xv, n), rem = xv % n;
-	} else {
-	    var rv = -div (-xv, n), rem = -((-xv) % n);
-	}
-
-	if (negative)
-	    rem = -rem;
-
-	return [new Scaled (rv), new Scaled (rem)];
-    };
-
-    proto.times_parts = function Scaled_times_parts (nonfrac, frac) {
-	nonfrac = nlib.maybe_unbox__O_I (nonfrac);
-	frac = nlib.maybe_unbox__O_I (frac);
-	var res = this.times_n_over_d (frac, SC_UNITY)[0];
-	return this.times_n_plus_y (nonfrac, res);
-    };
-
-    // Higher-level stuff.
-
-    proto.toString = function Scaled_toString () {
-	return '<~' + this.asfloat ().toFixed (6) + '|s>';
-    };
-
-    proto.clone = function Scaled_clone () {
-	return new Scaled (this.value_S);
-    };
-
-    proto.is_nonzero = function Scaled_is_nonzero () {
-	return (this.value_S != 0);
-    };
-
-    proto.as_int__I = function Scaled_as_int__I () {
-	return this.value_S; // Yes, this is correct.
-    };
-
-    proto.as_scaled__S = function Scaled_as_scaled__S () {
-	return this.value_S;
-    };
-
-    proto.as_glue = function Scaled_as_glue () {
-	return null;
-    };
-
-    proto.as_serializable = function Scaled_as_serializable () {
-	return this.value_S;
-    };
-
-    Scaled.deserialize = function Scaled_deserialize (data) {
-	return new Scaled (nlib.parse__O_I (data));
-    };
-
-    proto.advance = function Scaled_advance (other) {
-	return new Scaled (this.value_S + other.value_S);
-    };
-
-    proto.product__I_O = function Scaled_product__I_O (k) {
-	k = nlib.maybe_unbox__O_I (k);
-	return this.times_parts (k, 0);
-    };
-
-    proto.divide__I_O = function Scaled_divide__I_O (k) {
-	k = nlib.maybe_unbox__O_I (k);
-	return this.clone ().over_n (k)[0];
-    };
-
-    proto.asfloat = function Scaled_asfloat () {
-	return this.value_S * UNSCALE;
-    };
-
-    return Scaled;
-}) ();
-
-
 var Dimen = (function Dimen_closure () {
     var MAX_SCALED = 0x40000000; // 2**30 = 1073741824 = '10000000000
 
@@ -285,15 +76,6 @@ var Dimen = (function Dimen_closure () {
 
     inherit (Dimen, Value);
     var proto = Dimen.prototype;
-
-    Dimen.new_scaled = function Dimen_new_scaled (sp) {
-	if (!(sp instanceof Scaled))
-	    throw new TexInternalError ('expected Scaled value, got %o', sp);
-
-	var d = new Dimen ();
-	d.sp_S = sp.value_S;
-	return d;
-    };
 
     Dimen.new_product__IS_O = function Dimen_new_product__IS_O (k_I, x_S) {
 	var d = new Dimen ();
@@ -320,14 +102,12 @@ var Dimen = (function Dimen_closure () {
     };
 
     proto.set_to = function Dimen_set_to (val) {
-	if (val instanceof Scaled)
-	    this.sp_S = val.value_S;
-	else if (val instanceof Dimen)
+	if (val instanceof Dimen)
 	    this.sp_S = val.sp_S;
 	else if (typeof val === 'number')
 	    this.sp_S = val;
 	else
-	    throw new TexInternalError ('expected Scaled or Dimen value, got %o', val);
+	    throw new TexInternalError ('expected int or Dimen value, got %o', val);
 	return this;
     };
 
